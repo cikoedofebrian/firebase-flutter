65eb6b06075355463a366375e44042b24b046988
Ciko Edo Febrian <94286488+cikoedofebrian@users.noreply.github.com>
2023-01-15 23:52:30 +0700

    changes

--- app/lib/screens/home.dart
+++ app/lib/screens/home.dart
@@ -2,7 +2,6 @@ import 'package:app/widgets/chats.dart';
 import 'package:app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
